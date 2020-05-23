1cfb39979949b87287ec31e813e1b7790c69efa7
Alessandra <alessamica@gmail.com>
2020-05-23 00:07:48 -0400

    By Ale link, styles and tumblr link

--- src/Components/QuoteMachine.js
+++ src/Components/QuoteMachine.js
@@ -6,6 +6,7 @@ import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import Box from '@material-ui/core/Box';
 import IconButton from '@material-ui/core/IconButton';
+import Link from '@material-ui/core/Link';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTwitter } from '@fortawesome/fontawesome-free-brands';
@@ -50,51 +51,70 @@ const quoteLeftStyle = {
     color: '#f50057'
 };
 
+const byStyle = {
+    fontWeight: '300',
+    marginTop: '15px'
+
+};
+
 const QuoteMachine = ({ selectedQuote, assignNewQuoteIndex }) => (
-    <Card style={cardStyle}>
+    <Box>
+        <Card style={cardStyle}>
+            <Box 
+                display="block"
+                direction="row"
+                justify="center"
+                alignItems="center"
+            >
+                <Typography id="text" style={quoteStyles}>
+                    <FontAwesomeIcon 
+                        icon={faQuoteLeft} 
+                        style={quoteLeftStyle} 
+                        size="sm"
+                    />
+                    {selectedQuote.quote}<br />
+                </Typography>
+                <Typography style={authorStyle}>
+                    - <span id="author">{selectedQuote.author}</span>
+                </Typography>
+            </Box>
+            <CardActions style={footerStyles}>
+                <IconButton
+                    style={iconStyle} 
+                    id="tweet-quote"
+                    target="_blank" 
+                    href={`https://twitter.com/intent/tweet?text=${selectedQuote.quote}`}>
+                    <FontAwesomeIcon icon={ faTwitter} size="sm"/>
+                </IconButton>
+                <IconButton 
+                    style={iconStyle} 
+                    id="tumblr-quote"
+                    target="_blank" 
+                    href={`https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes&caption=${selectedQuote.author}+&content=+${selectedQuote.quote}+&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button`}>
+                    <FontAwesomeIcon icon={ faTumblr } size="sm"/>
+                </IconButton>
+                <Button 
+                    style={buttonStyle}
+                    variant="contained"
+                    color="secondary"
+                    id="new-quote"
+                    size="medium" 
+                    onClick={assignNewQuoteIndex}>Next Quote
+                </Button>
+            </CardActions>
+        </Card>
         <Box 
-            display="block"
-            direction="row"
-            justify="center"
-            alignItems="center"
-        >
-            <Typography id="text" style={quoteStyles}>
-                <FontAwesomeIcon 
-                    icon={faQuoteLeft} 
-                    style={quoteLeftStyle} 
-                    size="sm"
-                />
-                {selectedQuote.quote}<br />
-            </Typography>
-            <Typography style={authorStyle}>
-                - <span id="author">{selectedQuote.author}</span>
-            </Typography>
+            display="flex" 
+            justifyContent="center"
+            style={byStyle} >
+            by&nbsp;
+            <Link  
+                target="_blank" 
+                href="https://github.com/Alessag" 
+                color="textPrimary">Alessandra
+            </Link>
         </Box>
-        <CardActions style={footerStyles}>
-            <IconButton
-                style={iconStyle} 
-                id="tweet-quote"
-                target="_blank" 
-                href={`https://twitter.com/intent/tweet?text=${selectedQuote.quote}`}>
-                <FontAwesomeIcon icon={ faTwitter} size="sm"/>
-            </IconButton>
-            <IconButton 
-                style={iconStyle} 
-                id="tumblr-quote"
-                target="_blank" 
-                href={`https://tumblr.com/intent/tumblr?text=${selectedQuote.quote}`}>
-                <FontAwesomeIcon icon={ faTumblr } size="sm"/>
-            </IconButton>
-            <Button 
-                style={buttonStyle}
-                variant="contained"
-                color="secondary"
-                id="new-quote"
-                size="medium" 
-                onClick={assignNewQuoteIndex}>Next Quote
-            </Button>
-        </CardActions>
-    </Card>
+    </Box>
 );
 
 export default QuoteMachine;
